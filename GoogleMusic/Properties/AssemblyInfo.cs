006f2c382d0bc7e83cd54d4d094d5581c0047935
Lutz Bürkle <lutz.buerkle@web.de>
2013-06-29 15:54:19 +0200

    Minor update

--- GoogleMusic/Properties/AssemblyInfo.cs
+++ GoogleMusic/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("Jamcast.Plugins.GoogleMusic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GoogleMusic.NET")]
+[assembly: AssemblyDescription("GoogleMusic.NET is an unofficial API for Google Music")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("553a4801-a2d7-49c6-a78e-9847290d381e")]
+[assembly: Guid("1b79fe4a-1fb2-4b9b-81c6-16370a9b7365")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Buildnummer
 //      Revision
 //
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
